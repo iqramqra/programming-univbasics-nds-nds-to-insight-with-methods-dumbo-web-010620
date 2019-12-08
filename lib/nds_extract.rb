11a3d91452839ce8f0a05525d60bb2e22200aa2a
Iqra Masroor <masrooriqra@gmail.com>
2019-12-08 22:35:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -17,14 +17,9 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  grand_total =0
-  row_index = 0
-  while row_index<director_data.length
-  do
-    colum_index = 0
-    while colum_index < director_data.length do
-      grand_total += total_value_of_spinner(vm,row_index,colum_index)
-      colum_index +=1
-    end
-      row_index +=1
+ gross = 0
+ director_data[:movies].each do |n|
+   gross +=n[:worldwide_gross]
+ end
+ return gross
 end
